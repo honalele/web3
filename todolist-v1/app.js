5c3e6826f7f9c1c0f84b88c471f2df8c1cf990e7
honalele <dreamhona.lele@gmail.com>
2022-11-30 00:05:06 +0900

    add ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,8 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-	res.send("Hello");
+	var today = new Date();
+	var currentDay = today.getDay();
+	var day = "";
+
+	switch (currentDay) {
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Saturday";
+			break;
+		case 6:
+			day = "Sunday";
+			break;
+
+		default:
+			console.log("Error: CurrentDay is >6");
+			break;
+	}
+	res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
