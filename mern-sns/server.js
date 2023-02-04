810bd7eaf478f98a1f8c7a4220f7893513594490
honalele <dreamhona.lele@gmail.com>
2023-02-04 09:37:41 +0900

    game lp

--- mern-sns/server.js
+++ mern-sns/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
 
 app.get("/", (req, res) => {
 	res.send("Hello, express");
@@ -9,5 +11,7 @@ app.get("/", (req, res) => {
 
 //ミドルウェア
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
 
 app.listen(PORT, () => console.log("サーバーが起動しました！"));
