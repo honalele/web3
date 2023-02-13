708787fa47f8bcd2a1cc55b559f815f75cda0501
honalele <dreamhona.lele@gmail.com>
2023-02-13 12:41:47 +0900

    mern dev

--- mern-sns/server.js
+++ mern-sns/server.js
@@ -4,6 +4,18 @@ const PORT = 3000;
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//データベース接続
+mongoose
+	.connect(process.env.MONGOLURL)
+	.then(() => {
+		console.log("DBと接続中......");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 app.get("/", (req, res) => {
 	res.send("Hello, express");
