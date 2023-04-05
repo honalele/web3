298ad939022ab9dedfd9aecd194a371cf0d8746e
honalele <dreamhona.lele@gmail.com>
2023-04-05 18:47:15 +0900

    add function

--- web-camera/main.js
+++ web-camera/main.js
@@ -1,5 +1,36 @@
 $(function () {
-	function startVideo() {}
+	var myStream;
+	function startVideo() {
+		// Get the user media
+		navigator.mediaDevices
+			.getUserMedia({ video: true, audio: false })
+			.then(function (stream) {
+				var $video = $("#myVideo");
+				$video.attr("src", URL.createObjectURL(stream));
+				myStream = stream;
+			})
+			.catch(function (error) {
+				console.log("カメラの映像を取得できませんでした：", error);
+			});
+	}
 
-	$("#startVideo").click(startVideo);
+	function stopVideo() {
+		if (!myStream) {
+			return;
+		}
+		myStream.getVideoTracks().forEach(function (track) {
+			track.stop();
+		});
+		myStream = null;
+	}
+
+	function snapShot() {
+		if (!myStream) {
+			return;
+		}
+	}
+
+	$("#startVideo").click(startVideo).click();
+	$("#stopVideo").click(stopVideo);
+	$("#snapshot").click(snapshot);
 });
