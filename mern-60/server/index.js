d6b1a97eec4face381ed97a3805e528225a72b02
honalele <dreamhona.lele@gmail.com>
2023-05-03 21:21:46 +0900

    add mongoDB connection

--- mern-60/server/index.js
+++ mern-60/server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect();
+mongoose.connect(
+	"mongodb+srv://narenbao:YNFDpy4NpruYHHaY@cluster0.sscw8de.mongodb.net/test"
+);
 
 app.listen(3001, () => {
 	console.log("Server is running!");
