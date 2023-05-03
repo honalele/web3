54722fc2581db379ac33e31484e2e7879811f63a
honalele <dreamhona.lele@gmail.com>
2023-05-03 21:25:54 +0900

    mongodb password

--- mern-60/server/index.js
+++ mern-60/server/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 mongoose.connect(
-	"mongodb+srv://narenbao:YNFDpy4NpruYHHaY@cluster0.sscw8de.mongodb.net/test"
+	"mongodb+srv://narenbao:<password>@cluster0.sscw8de.mongodb.net/test"
 );
 
 app.listen(3001, () => {
